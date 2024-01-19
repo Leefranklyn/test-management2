bb2232cfeb7bed20f161cf0d3cd807aef629eefd
Leefranklyn <124352547+Leefranklyn@users.noreply.github.com>
2024-01-19 18:22:15 +0100

    Delete src/config/googleapi.js

--- src/config/googleapi.js
+++
@@ -1,9 +0,0 @@
-export const userDetails = {
-  user: "okpidugada@gmail.com",
-  pass: "C0h2rOZ35nKGySqQ",
-  clientId:
-    "106323593172-or5ksui6a0j5g6qirkq7vf6nt4e2gvbd.apps.googleusercontent.com",
-  clientSecret: "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",
-  redirectUrl: "https://developers.google/oauthplayground",
-  refreshToken: "1//04xS8r1TQi6mECgYIARAAGAQSNwF-L9Ir11ENjrCbqbNHeRhNbUeosF6FmplbAWnC3MihaQcf5f2FlXOGrzO_ZFsYcQs3irfPhzk",
-};
